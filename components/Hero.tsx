99844b64e6f69a35ca41152692be37187996df88
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-27 17:34:40 +0530

    button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>Hero
+    <div className='flex-1 pt-36 padding-x'><h1 className="her0_title">
+        Find, book, or rent a car - quickly and easily!
+    </h1>
+    <p className="hero_subtitle">
+        Streamline your car rental experience with our effortless booking process/
+    </p>
+    <CustomButton/>
+    </div>
+    </div>
   )
 }
 
