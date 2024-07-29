6c6c20384217b36a29d44befa933b59931a04bd6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-29 22:51:57 +0530

    added-files

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { useState } from 'react'
+
 import React from 'react'
+import SearchManufacturer from './SearchManufacturer'
 
 const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('');
+    const handleSearch = () => {}
   return (
-    <div>SearchBar</div>
+
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className='searchbar__item'>
+          <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+          />
+        </div>
+    </form>
   )
 }
 
