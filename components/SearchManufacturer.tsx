3c808a7ed6ec841d5a3f1ebf6288fdc00aea9dee
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-29 23:20:36 +0530

    started-adding manufacturers

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+"use client"
+import  { useState, Fragment } from 'react'
+import { SearchManuFacturerProps } from "@/types"
+import { Combobox, Transition } from "@headlessui/react"
+import  {  manufacturers } from '@/constants/index'
+import Image from "next/image"
+const SearchManufacturer = ( { manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
+    const [query, setQuery] = useState('');
 
-const SearchManufacturer = ( { manufacturer, setManufacturer }: ) => {
+    const filteredManufacturers = query === "" ? manufacturer : manufacturers.filter((item) => (
+        
+    ))
+
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+        <Combobox>
+            <div className="relative w-full">
+             <Combobox.Button className="absolute top-[14px]">
+             <Image
+             src="/car-logo.svg"
+             width={20}
+             height={20}
+             className="ml-4"
+             alt="car logo"
+             />
+             </Combobox.Button>
+
+             <Combobox.Input
+             className="search-manufacturer__input"
+             placeholder="Volkswagen"
+             displayValue={(manufacturer: string) => manufacturer}
+             onChange={(e) => setQuery(e.target.value)}
+             />
+
+             <Transition
+             as={Fragment}
+             leave='transition ease-in duration-100'
+             leaveFrom='opacity-100'
+             afterLeave={() => setQuery('')}
+             >
+                <Combobox.Options>
+
+                </Combobox.Options>
+             </Transition>
+            </div>
+        </Combobox>
+        </div>
   )
 }
 
